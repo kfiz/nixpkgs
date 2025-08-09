3a480cef9e3ba6fee9b07fcb7b91c34a034a91bb
Jappie3 <jappie3+git@jappie.dev>
2025-08-09 16:37:20 +0200

    dovecot_pigeonhole: patch regarding permission error
    This patch fixes a permission issue that occurs when saving compiled
    sieve scripts sourced from the nix store. Instead of reusing the
    read-only permission bits from the nix store, it explicitly uses `0700`
    for the directory in which compiled sieve scripts should be saved.
    
    Additional context:
    - https://github.com/NixOS/nixpkgs/pull/388463#issuecomment-3066016707
    - https://github.com/dovecot/pigeonhole/pull/15

--- pkgs/by-name/do/dovecot_pigeonhole/package.nix
+++ pkgs/by-name/do/dovecot_pigeonhole/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   callPackage,
+  fetchpatch,
   dovecot,
 }:
 let
@@ -51,6 +52,12 @@ callPackage ./generic.nix { } rec {
   url = "https://pigeonhole.dovecot.org/releases/${lib.versions.majorMinor dovecot.version}/dovecot-pigeonhole-${version}.tar.gz";
   version = "2.4.0";
   hash = "sha256-DtCK4WOsOalEcgD7tC17OwXTXpHZmBjdD0r9etHbx1M=";
+  patches = [
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/dovecot/pigeonhole/pull/15.patch";
+      sha256 = "sha256-BLBz9ZhOGEIIitnXG0uM6bZBRNnQBy4K2IJlh1+Un50=";
+    })
+  ];
 
   inherit dovecot;
 }
